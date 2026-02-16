a9ad66f4086efea68de7ad8e436cef8ba3b67637
aaronw3355 <avisualofficial@gmail.com>
2026-02-16 13:16:14 -0500

    Update TermProject.py

--- TermProject.py
+++ TermProject.py
@@ -15,6 +15,33 @@ def save_data(filename, data):
     with open(filename, "w") as file:
         json.dump(data, file, indent=4)
 
+def add_meal(meals):
+    food = input("Food name: ")
+    calories = int(input("Calories: "))
+    protein = int(input("Protein (g): "))
+    carbs = int(input("Carbs (g): "))
+    fat = int(input("Fat (g): "))
+
+    meals.append({
+        "food": food,
+        "calories": calories,
+        "protein": protein,
+        "carbs": carbs,
+        "fat": fat
+    })
+
+def add_workout(workouts):
+    exercise = input("Exercise name: ")
+    duration = int(input("Duration (minutes): "))
+    calories = int(input("Calories burned: "))
+
+    workouts.append({
+        "exercise": exercise,
+        "duration": duration,
+        "calories": calories
+    })
+
+
 def main():
     pass
 
