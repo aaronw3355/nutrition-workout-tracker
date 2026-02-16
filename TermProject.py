13be6849b55eea0a56cf1699c32754ea8c6ad253
aaronw3355 <avisualofficial@gmail.com>
2026-02-16 12:48:36 -0500

    Update TermProject.py

--- TermProject.py
+++ TermProject.py
@@ -4,6 +4,17 @@ MEAL_FILE = "meals.json"
 WORKOUT_FILE = "workouts.json"
 CALORIE_GOAL_FILE = "calorie_goal.json"
 
+def load_data(filename):
+    try:
+        with open(filename, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+def save_data(filename, data):
+    with open(filename, "w") as file:
+        json.dump(data, file, indent=4)
+
 def main():
     pass
 
